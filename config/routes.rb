e9da2a4ca3fa7d947c03fdf18fcb18061ca5ea73
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-02-20 14:53:35 +0100

    tasks

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,17 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # get "/tasks", to: "tasks#index"
+  # get "/tasks/new", to: "tasks#new", as: :new_task
+  # get "/tasks/:id", to: "tasks#show", as: :task
+  # post "/tasks", to: "tasks#create"
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
+  # delete "/tasks/:id", to: "tasks#destroy", as: :d_task
+  resources :tasks
+
+  # delete "tasks/:id", to: "tasks#destroy"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
